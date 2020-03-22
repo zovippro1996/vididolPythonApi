88090f1335852804146416999a2878e30bb04eb1
zovippro1996 <trunglepham1202@gmail.com>
2020-03-22 18:53:12 +0700

    Set up DBmodel and apps

--- backendPy/backendPy/settings.py
+++ backendPy/backendPy/settings.py
@@ -31,12 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'webserviceapi.apps.WebserviceapiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +50,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:8000',
+    "http://localhost:8080",
 ]
 
 ROOT_URLCONF = 'backendPy.urls'
@@ -75,11 +84,16 @@ WSGI_APPLICATION = 'backendPy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'vididolstagingdb',
+        'USER': 'vididoladmin',
+        'PASSWORD': 'vididolAdmin123@',
+        'HOST': 'db4free.net',
+        'PORT': '3306',
     }
 }
 
+AUTH_USER_MODEL = 'webserviceapi.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
--- backendPy/webserviceapi/models.py
+++ backendPy/webserviceapi/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+
+class User(AbstractUser):
+    """This is a class for keeping general User Account
+
+    Arguments:
+        AbstractUser {class} -- Django Class
+    """
+    email = models.EmailField(
+        verbose_name="User's personal email", max_length=200, unique=True)
+    facebook_link = models.URLField(
+        verbose_name="User's Facebook link", max_length=250)
+    instagram_link = models.URLField(
+        verbose_name="User's Instagram link", max_length=250)
+    is_email_activated = models.BooleanField(
+        verbose_name="check User email activated", default=False)
+    last_updated_time = models.DateTimeField(
+        verbose_name="time User record last changed", auto_now=True)
+    created_time = models.DateTimeField(
+        verbose_name="time User record created", auto_now_add=True)
+
+
+class Star(User):
+    """This class help identify Star Account
+
+    Arguments:
+        User {class} -- Base User Class
+    """
+    is_certified = models.BooleanField(
+        verbose_name="check Star's certified", default=False)
+    response_rate = models.FloatField(
+        verbose_name="Star's Response Rate", null=True)
