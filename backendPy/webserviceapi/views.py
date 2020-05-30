b2a9b4b50bf491d5cc9546419029133207a0d61c
zovippro1996 <trunglepham1202@gmail.com>
2020-05-30 22:31:30 +0700

    Update login API  function

--- backendPy/webserviceapi/views.py
+++ backendPy/webserviceapi/views.py
@@ -4,10 +4,9 @@ from rest_framework import viewsets, status
 from rest_framework.parsers import JSONParser
 from django.http.response import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from backendPy.webserviceapi.models import User, Star
-from backendPy.webserviceapi.serializers import UserSerializer, StarSerializer
-
-# Create your views here.
+from backendPy.webserviceapi.models import Account
+from backendPy.webserviceapi.serializers import AccountSerializer
+from django.contrib.auth import authenticate, login
 
 
 def index(request):
@@ -23,29 +22,67 @@ def star_list(request):
     """
     # GET - Retrieve list of Stars
     if request.method == 'GET':
-        stars = Star.objects.all()
-        stars_serializer = StarSerializer(stars, many=True)
-        return JsonResponse(stars_serializer.data, safe=False, status=status.HTTP_200_OK)
+        stars = Account.objects.get(account_is_star=True)
+        star_serializer = AccountSerializer(stars, many=True)
+        return JsonResponse(star_serializer.data, safe=False, status=status.HTTP_200_OK)
+
+    # Add one
+    if request.method == 'POST':
+        star_data = JSONParser().parse(request)
+        star_serializer = AccountSerializer(data=star_data)
+        if star_serializer.is_valid():
+            star_serializer.save()
+            return JsonResponse(star_serializer.data, status=status.HTTP_201_CREATED)
+        return JsonResponse(star_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'DELETE':
+        Account.objects.get(account_is_star=True).delete()
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
 
 
 @csrf_exempt
 def star_detail(request, pk):
-    """Manipulate one star based on request
-
-    Arguments:
-        request {HttpRequest} -- request from client
-        pk {identifier} -- Primary Key
-
-    Returns:
-        JSONResponse -- Result after operation
-    """
-    # getStar from Primary Key
     try:
-        star = Star.objects.get(pk=pk)
-    except Star.DoesNotExist as notfound:
+        star = Account.objects.get(pk=pk)
+    except Account.DoesNotExist:
         return HttpResponse(status=status.HTTP_404_NOT_FOUND)
 
-     # GET - Retrieve one record
+    # Retrieve one record
     if request.method == 'GET':
-        star_serializer = StarSerializer(star)
-        return JsonResponse(star_serializer.data, status=status.HTTP_200_OK)
+        star_serializer = AccountSerializer(star)
+        return JsonResponse(star_serializer.data)
+
+    # Update one record
+    if request.method == 'PUT':
+        star_data = JSONParser().parse(request)
+        star_serializer = AccountSerializer(star, data=star_data)
+        if star_serializer.is_valid():
+            star_serializer.save()
+            return JsonResponse(star_serializer.data)
+        return JsonResponse(star_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # Delete one record
+    if request.method == 'DELETE':
+        star.delete()
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+
+
+@csrf_exempt
+def user_signin(request):
+    # Sign In with Username and Password
+    if request.method == 'POST':
+        signin_data = JSONParser().parse(request)
+        signin_serializer = AccountSerializer(data=signin_data)
+        if signin_serializer.is_valid():
+            validUser = authenticate(
+                username=signin_serializer.data.username,
+                password=signin_serializer.data.username
+            )
+
+            if validUser is not None:
+                login(request, validUser)
+                return JsonResponse(status=status.HTTP_200_OK)
+            else:
+                return JsonResponse(status=status.HTTP_401_UNAUTHORIZED)
+
+        return JsonResponse(signin_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
