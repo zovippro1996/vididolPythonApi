ce95a07d52414b0cb3efba7de7828ce5c59ea0e4
zovippro1996 <trunglepham1202@gmail.com>
2020-06-10 23:56:35 +0700

    Refresh

--- backendPy/webserviceapi/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from webserviceapi import views
-
-urlpatterns = [
-    path("stars/", views.star_list),
-    path("stars/<int:pk>/", views.star_detail),
-    path("fanrequest/", views.fanrequest_list),
-    path("fanrequest/<int:pk>/", views.fanrequest_detail)
-]
--- env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\trung\documents\mycode\projectcode\vididol-gitlab\python-backend\env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
