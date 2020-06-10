95632f66c2c7890d3e012ca7297ee81540859c96
zovippro1996 <trunglepham1202@gmail.com>
2020-06-11 01:47:33 +0700

    Test commit

--- backendPy/webserviceapi/urls.py
+++ backendPy/webserviceapi/urls.py
@@ -1,9 +1,22 @@
 from django.urls import path
 from webserviceapi import views
+from django.conf.urls import url
+from django.views.generic import TemplateView
+from rest_framework_swagger.views import get_swagger_view
 
+schema_view = get_swagger_view(title='Vididol API')
 urlpatterns = [
     path("stars/", views.star_list),
     path("stars/<int:pk>/", views.star_detail),
     path("fanrequest/", views.fanrequest_list),
-    path("fanrequest/<int:pk>/", views.fanrequest_detail)
+    path("fanrequest/<int:pk>/", views.fanrequest_detail),
+    path('swagger-ui/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url': 'openapi-schema'}
+    ), name='swagger-ui'),
+        path('openapi', get_schema_view(
+        title="Your Project",
+        description="API for all things …",
+        version="1.0.0"
+    ), name='openapi-schema'),
 ]
