2381656aef878854fcfc512a41d6089e044b3fb2
zovippro1996 <trunglepham1202@gmail.com>
2020-08-08 15:46:45 +0700

    Update admin page

--- webserviceapi/admin.py
+++ webserviceapi/admin.py
@@ -1,15 +1,23 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Account, UserInformation, FanRequest, Star
+from .models import *
 
 # Register your models here.
 # admin.site.register(Account, UserAdmin)
 
+
 class FullUserAdmin(admin.ModelAdmin):
     pass
+
 
 # Register your models here.
 admin.site.register(UserInformation)
 admin.site.register(FanRequest)
 admin.site.register(Account)
 admin.site.register(Star, FullUserAdmin)
+admin.site.register(PostComment)
+admin.site.register(Video)
+admin.site.register(VideoLengthOptions)
+admin.site.register(Post)
+admin.site.register(Notification)
+admin.site.register(Log)
